321d4a6b20eff115893e79bcc88c5ba6fe618597
kNUT1175 <atrainseattle@gmail.com>
2025-09-09 15:38:38 -0600

    start

--- Project1_Sudoku/Code/main.py
+++ Project1_Sudoku/Code/main.py
@@ -7,3 +7,16 @@ f = open("Easy-P1.txt")
 print(f.read)
 
 print("Hello")
+
+class Thing:
+  def __init__(self, number, permanent, grid, spot):
+    self.number = number
+    self.permanent = permanent
+    self.grid = grid
+    self.spot = spot
+
+
+p1 = Thing("John", 36)
+
+print(p1.name)
+print(p1.age)
