ba4e075da4164b215cb33c7067abf290df2ccfe8
kNUT1175 <atrainseattle@gmail.com>
2025-09-09 17:20:12 -0600

    adding...

--- Project1_Sudoku/Code/main.py
+++ Project1_Sudoku/Code/main.py
@@ -2,6 +2,8 @@
 # Date: 8_27_25
 # Program: Sudoku Puzzle
 # Class: CSCI 446
+from ftplib import print_line
+
 import numpy as np
 
 
@@ -21,9 +23,30 @@ class Thing:
 #print(p1.number)
 #print(p1.permanent)
 
-f = open("../Puzzles/Easy-P1.txt")
+file = open("../Puzzles/Easy-P1.txt", encoding="utf-8-sig")
 
-puzzle = np.zeros((9, 9), dtype=Thing)
-print(puzzle)
+puzzle = np.zeros((9, 9), dtype=object)
+counter = 0
+grid = 0
+rowSPot = 0
+for row in file:
+    line = row.strip().split(",")
+    colSpot = 0
+    for val in line:
+        if val == "?":
+            num = 0
+        else:
+            num = int(val)
+        if num != 0:
+            puzzle[rowSPot][colSpot] = Thing(num, True, rowSPot, colSpot, grid)
+        else:
+            puzzle[rowSPot][colSpot] = Thing(num, False, rowSPot, colSpot, grid)
+        colSpot = colSpot + 1
+    rowSPot += 1
+
+for i in range(9):
+    for j in range(9):
+        print(i, j, puzzle[i][j].number)
+
 
 
