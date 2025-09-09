dd16377e0e2067efddf93eb5aaa6cc151aa50184
sethorater <118314693+sethorater@users.noreply.github.com>
2025-09-09 17:30:56 -0600

    adjusted some stuff

--- Project1_Sudoku/Code/main.py
+++ Project1_Sudoku/Code/main.py
@@ -2,26 +2,9 @@
 # Date: 8_27_25
 # Program: Sudoku Puzzle
 # Class: CSCI 446
-from ftplib import print_line
 
 import numpy as np
-
-
-
-print("This works!")
-
-class Thing:
-  def __init__(self, number, permanent, row, column, grid):
-    self.number = number
-    self.permanent = permanent
-    self.row = row
-    self.column = column
-    self.grid = grid
-
-#p1 = Thing(25, True, 26, 6)
-
-#print(p1.number)
-#print(p1.permanent)
+from class_thing import Thing
 
 file = open("../Puzzles/Easy-P1.txt", encoding="utf-8-sig")
 
